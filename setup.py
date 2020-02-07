37ed5a523eab1a75a8b2a943f82889dafb5569b2
Diego Alejandro Asencio Cuellar <diego.asencio@rappi.com>
2020-02-06 23:34:16 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='django-rest-users',
+    packages=['django-rest-users'],
     version='0.0.1',
     description='Basic Endpoints REST API for User Models',
     author='Diego A Asencio C',
     license='MIT',
     author_email='diegoasencio96@gmail.com',
-    url='http://www.diegoasencio.co',
-    download_url='http://www.diegoasencio.co',
+    url='https://github.com/diegoasencio96/django-rest-users/',
+    download_url='https://github.com/diegoasencio96/django-rest-users/releases/tag/0.0.1',
     keywords=['django-rest-users', 'rest', 'users'],
     include_package_data=True,
     zip_safe=True,
